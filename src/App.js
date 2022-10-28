2b93c942f97e527d56d183de45de59f13975b28f
ridoy113 <almamounhridoy113@gmail.com>
2022-10-28 15:41:06 +0600

    add some packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <h1>fhsafpiouashf</h1>
+      <h1 className="text-3xl">
+        Hello world!
+      </h1>
+      <Routes>
+        <Route path="/" element="" />
+      </Routes>
+
     </div>
   );
 }
