ceb9534690a46cd0772f28882f645479a52dbfb7
ridoy113 <almamounhridoy113@gmail.com>
2022-10-28 15:46:04 +0600

    navbar making start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './components/shaered/navbar/Navbar';
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl">
         Hello world!
       </h1>
