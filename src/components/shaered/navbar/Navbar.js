e6d52b08236205d58a916b54808f3cf3457e7016
ridoy113 <almamounhridoy113@gmail.com>
2022-10-28 18:53:43 +0600

    navbar make responsive'

--- src/components/shaered/navbar/Navbar.js
+++ src/components/shaered/navbar/Navbar.js
@@ -15,19 +15,33 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 w-[12rem] md:w-[16rem]">
+                            <li><Link className='text-stone-700 text-[1.2rem] hover:text-[#FA471C] font-normal' to="/">HOME</Link></li>
+                            <hr />
+
+
                             <li tabIndex={0}>
                                 <a className="justify-between">
-                                    Parent
+                                    <Link className='text-stone-700 text-[1.2rem] hover:text-[#FA471C] font-normal' to="/">PAGES</Link>
+                                    <hr />
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" ><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                 </a>
                                 <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                    <li>
+                                        <Link className='hover:text-[#FA471C]' to="">About Us</Link>
+                                        <hr />
+                                        <Link className='hover:text-[#FA471C]' to="">About Me</Link>
+                                        <hr />
+                                        <Link className='hover:text-[#FA471C]' to="">Our Process</Link>
+                                        <hr />
+                                        <Link className='hover:text-[#FA471C]' to="">Contact Us</Link>
+                                    </li>
+
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+
+                            
+
                         </ul>
                     </div>
                     <img className="w-auto md:w-[178px] lg:w-[178px] lg:ml-8" src={logo} alt="" />
@@ -38,11 +52,13 @@ const Navbar = () => {
                 <div className="dropdown dropdown-hover navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
                         <li><Link className='text-white hover:text-[#FA471C] font-bold' to="/">HOME</Link></li>
+
+
                         <li tabIndex={0}>
                             <a>
                                 <span className='text-white hover:text-[rgb(250,71,28)] font-bold'>PAGES</span>
                             </a>
-                            <ul className="p-2 bg-black border-t-2 border-[#FA471C] p-[1.5rem] pr-[4.4rem]">
+                            <ul className="p-2 bg-stone-900 bg-opacity-50 border-t-2 border-[#FA471C] p-[1.5rem] pr-[4.4rem]">
                                 <li>
                                     <Link className='hover:text-white' to="">About Us</Link>
                                     <Link className='hover:text-white' to="">About Me</Link>
@@ -51,6 +67,8 @@ const Navbar = () => {
                                 </li>
                             </ul>
                         </li>
+
+                        
                     </ul>
                 </div>
                 <div className="navbar-end mr-[2rem]">
