96344521f8e3e3da0dbc0c85bd40293e652f9933
ridoy113 <almamounhridoy113@gmail.com>
2022-11-01 15:03:00 +0600

    make difference section complite

--- src/components/home/homeComponents/Difference.js
+++ src/components/home/homeComponents/Difference.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import difImg from '../assets/h1-img-5.jpg'
+import textbgImg from '../assets/titlebackground-image-2.png'
 
 
 
 const Difference = () => {
     return (
         <div>
-            <div className='bg-[#F2F1F1] py-[3rem] px-[3rem] md:[4rem] md:px-[6rem] lg:py-[5rem] lg:px-[7.5rem] md:grid grid-cols-3 lg:grid grid-cols-3 gap-[1.5rem]'>
-                <div className='grid content-center'>
-                    <h1>MAKE A DIFFERENCE.</h1>
-                    <h6></h6>
-                    <p>Lorem ipsum dolor sit amet, adipiscing elit, consect adipiscing elit. Duis ut ligula leo eti. Aliquam suscip sed purus. Lorem ipsum dolor, consectetur dolor sit ut ligula leo. Aliquam suscip.</p>
+            <div className='bg-[#F2F1F1] py-[3rem] px-[3rem] md:[4rem] md:px-[6rem] lg:py-[5rem] lg:px-[7.5rem] lg:grid grid-cols-3 gap-[1.5rem]'>
+                <div className='grid content-center pb-[3.5rem]'>
+
+
+                    <div>
+                        <h1 className='text-[2.8rem] font-bold text-black leading-[3rem]'>MAKE A DIFFERENCE.</h1>
+                    </div>
+
+
+                    <h6 className='h-[0.2rem] w-[6rem] bg-[#f9471c] my-[1.5rem]'></h6>
+
+                    <p className='tracking-wide text-black'>Lorem ipsum dolor sit amet, adipiscing elit, consect adipiscing elit. Duis ut ligula leo eti. Aliquam suscip sed purus. Lorem ipsum dolor, consectetur dolor sit ut ligula leo. Aliquam suscip.</p>
                 </div>
-                <div className='md:col-span-2 lg:col-span-2'>
+                <div className='lg:col-span-2'>
                     <img src={difImg} alt="" />
                 </div>
             </div>
--- src/components/home/homeHeader/HomeHeader.js
+++ src/components/home/homeHeader/HomeHeader.js
@@ -7,7 +7,6 @@ import { Navigation } from "swiper";
 import hImg1 from '../assets/h1-slider-1-background-img.jpg'
 import hImg2 from '../assets/h1-slider-2-background-img.jpg'
 import hImg3 from '../assets/h1-slider-3-background-img.jpg'
-import hText from '../assets/titlebackground-image-2.png'
 
 
 
@@ -20,18 +19,14 @@ const HomeHeader = () => {
                 <SwiperSlide>
                     <div className=" hero min-h-screen" style={{ backgroundImage: `url(${hImg1})` }}>
                         <div className="text text-center">
-                            <h1 className='text-[50px] lg:text-[90px] font-bold text-white leading-[3.5rem] lg:leading-[5.4rem]'>
+                            <h1 className='text-[30px] lg:text-[90px] font-bold text-white leading-[2rem] lg:leading-[5.4rem]'>
                                 LACE UP AND
-
-                                <div className='bg-no-repeat bg-center' style={{ backgroundImage: `url(${hText})` }}>
-                                    <div className="text-[#FA471C]">
-                                        DON'T
-                                    </div>
+                                <div className="text-[#FA471C]">
+                                    DON'T
                                 </div>
-
                                 STOP.</h1>
                             <div className='flex justify-center'>
-                                <p className='h-[6px] w-[18rem] bg-white my-[1.8rem] '></p>
+                                <p className='h-[6px] w-[10rem] lg:h-[6px] lg:w-[18rem] bg-white my-[1.8rem]'></p>
                             </div>
                             <button className='border-solid border-[1px] border-white w-[12rem] h-[3.5rem] hover:w-[12.5rem] hover:h-[4rem] ease-in-out duration-300'>
                                 <button className='w-[11.5rem] h-[3rem] bg-white text-black font-bold'>BUY THEME</button>
@@ -44,18 +39,14 @@ const HomeHeader = () => {
                 <SwiperSlide>
                     <div className=" hero min-h-screen" style={{ backgroundImage: `url(${hImg2})` }}>
                         <div className="text text-center">
-                            <h1 className='text-[50px] lg:text-[90px] font-bold text-white leading-[3.5rem] lg:leading-[5.4rem]'>
+                            <h1 className='text-[30px] lg:text-[90px] font-bold text-white leading-[2rem] lg:leading-[5.4rem]'>
                                 WE
-
-                                <div className='bg-no-repeat bg-center' style={{ backgroundImage: `url(${hText})` }}>
-                                    <div className="text-[#FA471C]">
-                                        RUN
-                                    </div>
+                                <div className="text-[#FA471C]">
+                                    RUN
                                 </div>
-
                                 WE BECOME.</h1>
                             <div className='flex justify-center'>
-                                <p className='h-[6px] w-[18rem] bg-white my-[1.8rem] '></p>
+                                <p className='h-[6px] w-[10rem] lg:h-[6px] lg:w-[18rem] bg-white my-[1.8rem]'></p>
                             </div>
                             <button className='border-solid border-[1px] border-white w-[12rem] h-[3.5rem] hover:w-[12.5rem] hover:h-[4rem] ease-in-out duration-300'>
                                 <button className='w-[11.5rem] h-[3rem] bg-white text-black font-bold'>BUY THEME</button>
@@ -68,18 +59,14 @@ const HomeHeader = () => {
                 <SwiperSlide>
                     <div className=" hero min-h-screen" style={{ backgroundImage: `url(${hImg3})` }}>
                         <div className="text text-center">
-                            <h1 className='text-[50px] lg:text-[90px] font-bold text-white leading-[3.5rem] lg:leading-[5.4rem]'>
+                            <h1 className='text-[30px] lg:text-[90px] font-bold text-white leading-[2rem] lg:leading-[5.4rem]'>
                                 COUNQUER
-
-                                <div className='bg-no-repeat bg-center' style={{ backgroundImage: `url(${hText})` }}>
-                                    <div className="text-[#FA471C]">
-                                        ANY
-                                    </div>
+                                <div className="text-[#FA471C]">
+                                    ANY
                                 </div>
-
                                 TERRAIN.</h1>
                             <div className='flex justify-center'>
-                                <p className='h-[6px] w-[18rem] bg-white my-[1.8rem] '></p>
+                                <p className='h-[6px] w-[10rem] lg:h-[6px] lg:w-[18rem] bg-white my-[1.8rem]'></p>
                             </div>
                             <button className='border-solid border-[1px] border-white w-[12rem] h-[3.5rem] hover:w-[12.5rem] hover:h-[4rem] ease-in-out duration-300'>
                                 <button className='w-[11.5rem] h-[3rem] bg-white text-black font-bold'>BUY THEME</button>
