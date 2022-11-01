930fc8a0d2ca278cee67d3cc4505509544185e6f
ridoy113 <almamounhridoy113@gmail.com>
2022-11-01 14:05:10 +0600

    make difference page start

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Difference from './homeComponents/Difference';
 import HomeHeader from './homeHeader/HomeHeader';
 
 const Home = () => {
     return (
         <div>
             <HomeHeader />
+            <Difference />
         </div>
     );
 };
