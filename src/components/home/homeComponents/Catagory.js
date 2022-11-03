8fa40bfa09445bb6528d6cfdbb06e5d5cbbff799
ridoy113 <almamounhridoy113@gmail.com>
2022-11-03 09:30:40 +0600

    catarory logo making

--- src/components/home/homeComponents/Catagory.js
+++ src/components/home/homeComponents/Catagory.js
@@ -14,8 +14,8 @@ const Catagory = () => {
         <div>
             <div className='py-[3rem] px-[3rem] md:[4rem] md:px-[6rem] lg:py-[5rem] lg:px-[7.5rem] lg:grid grid-cols-3 gap-[1.5rem]'>
 
-                <div className='flex'>
-                    <p><GiStarGate /></p>
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-4xl lg:text-5xl'><GiStarGate /></p>
                     <div>
                         <h3>WORKING HOURS</h3>
                         <p>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
