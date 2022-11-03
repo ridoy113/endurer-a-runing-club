4593287c2ad18793753ffee68a31a475247d9c85
ridoy113 <almamounhridoy113@gmail.com>
2022-11-03 09:43:30 +0600

    first catagory complite

--- src/components/home/homeComponents/Catagory.js
+++ src/components/home/homeComponents/Catagory.js
@@ -15,10 +15,10 @@ const Catagory = () => {
             <div className='py-[3rem] px-[3rem] md:[4rem] md:px-[6rem] lg:py-[5rem] lg:px-[7.5rem] lg:grid grid-cols-3 gap-[1.5rem]'>
 
                 <div className='flex py-[2.5rem]'>
-                    <p className='text-4xl lg:text-5xl'><GiStarGate /></p>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><GiStarGate /></p>
                     <div>
-                        <h3>WORKING HOURS</h3>
-                        <p>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>WORKING HOURS</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
                     </div>
                 </div>
                 <div>2</div>
