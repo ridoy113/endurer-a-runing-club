2931d6ac7a98c4ca1109591adfda5f8b33e8dfc8
ridoy113 <almamounhridoy113@gmail.com>
2022-11-03 09:49:17 +0600

    make all catagory complite

--- src/components/home/homeComponents/Catagory.js
+++ src/components/home/homeComponents/Catagory.js
@@ -21,11 +21,47 @@ const Catagory = () => {
                         <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
                     </div>
                 </div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
+
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><SlPeople /></p>
+                    <div>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>OUR HISTORY</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                    </div>
+                </div>
+
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><MdDeveloperMode /></p>
+                    <div>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>WEB DEVELOPMENT</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                    </div>
+                </div>
+
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><AiOutlineHeart /></p>
+                    <div>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>PERFECT DESIGN</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                    </div>
+                </div>
+
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><VscGraphLine /></p>
+                    <div>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>ROUTES AVAILABLE</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                    </div>
+                </div>
+
+                <div className='flex py-[2.5rem]'>
+                    <p className='text-5xl mr-[1rem] mt-[-0.5rem] text-zinc-400 hover:text-[#f9471c] ease-in duration-300 cursor-pointer	'><ImHourGlass /></p>
+                    <div>
+                        <h3 className='text-[1.4rem] font-bold text-black pb-[0.7rem] hover:text-[#f9471c] ease-in duration-300 cursor-pointer'>TAKE A TOUR</h3>
+                        <p className='tracking-wider'>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean com ligula eget.</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
