9ab63be11873402d0f706cbaeb01b25ec0b8b16a
ridoy113 <almamounhridoy113@gmail.com>
2022-11-07 17:27:57 +0600

    news gird make

--- src/components/home/homeComponents/News.js
+++ src/components/home/homeComponents/News.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const News = () => {
     return (
-        <div className='px-[3rem] lg:px-[7.5rem]'>
+        <div className='pb-[3rem] lg:pb-[7.5rem] px-[3rem] lg:px-[7.5rem]'>
             <div className='text-center text-4xl font-bold text-black'>
                 <p>OUR NEWS.</p>
                 <div className='flex justify-center'>
@@ -12,8 +12,12 @@ const News = () => {
 
 
 
-            <div className='grid grid-cols-1 lg:grid lg:grid-cols-3'>
-                <div>1</div>
+            <div className='grid grid-cols-1 lg:grid lg:grid-cols-3 text-center gap-8 mt-[2rem] lg:mt-[4rem]'>
+                
+                <div>
+
+                </div>
+
                 <div>1</div>
                 <div>1</div>
             </div>
