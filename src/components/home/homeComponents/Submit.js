37dbbd90aaafd06bdfda58282a42aad1e6a97a8e
ridoy113 <almamounhridoy113@gmail.com>
2022-11-08 15:34:27 +0600

    submit section complite

--- src/components/home/homeComponents/Submit.js
+++ src/components/home/homeComponents/Submit.js
@@ -4,7 +4,20 @@ const Submit = () => {
     return (
         <div>
             <div className='bg-[#FA471C] py-[3rem] lg:py-[5rem] px-[3rem] lg:px-[7.5rem] text-center'>
-                <h1 className='text-[#fff] text-2xl font-semibold'>SIGN UP FOR OUR WEEKLY NEWSLETTER</h1>
+                <h1 className='text-[#fff] text-2xl font-semibold tracking-wide'>SIGN UP FOR OUR WEEKLY NEWSLETTER</h1>
+
+                <div className='flex-none lg:flex justify-center my-[2rem]'>
+                    <div>
+                        <input type="text" placeholder="Enter Your Email..." className="bg-[#FA471C] input border-solid border-[1px] border-[#fff] rounded-none text-[#fff]
+                        w-[20rem] h-[3rem] lg:w-[27rem] lg:h-[3.5rem] mx-[1rem]
+                        my-[1rem] lg:my-[0rem]" />
+                    </div>
+
+
+                    <button className='border-solid border-[1px] border-white w-[10.5rem] h-[3rem] lg:w-[12rem] lg:h-[3.5rem] hover:w-[11rem] hover:h-[3.2rem] hover:lg:w-[12.5rem] hover:lg:h-[4rem] ease-in-out duration-300'>
+                        <button className='w-[9.9rem] h-[2.4rem] lg:w-[11.5rem] lg:h-[3rem] bg-white text-black font-bold'>SUBMIT</button>
+                    </button>
+                </div>
             </div>
         </div>
     );
