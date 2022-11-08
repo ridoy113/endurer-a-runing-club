e6898bd19f3c37b14b9b331b15484ef604ecd424
ridoy113 <almamounhridoy113@gmail.com>
2022-11-08 15:51:58 +0600

    footer logo add

--- src/components/shaered/footer/Footer.js
+++ src/components/shaered/footer/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import footerLogo from './assets/Logo-1x1-white-1.png';
+import footerMap from './assets/footer-map.png';
+
+
 
 const Footer = () => {
     return (
         <div>
-            <div className='bg-black'>
+            <div className='bg-black py-[3rem] lg:py-[5rem] px-[2.5rem] lg:px-[7.5rem] grid grid-cols-1 lg:grid lg:grid-cols-4'>
+
+                <div>
+                    <img src={footerLogo} alt="" />
+                </div>
+
+                <div>1</div>
+
+                <div>1</div>
+
+                <div>1</div>
 
             </div>
         </div>
