3f3a0b6b51fc121d76f7b17c2c00c6517befbd4d
ridoy113 <almamounhridoy113@gmail.com>
2022-11-11 17:20:25 +0600

    our process info1 complite

--- src/components/ourProcess/OurProcess.js
+++ src/components/ourProcess/OurProcess.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../shaered/footer/Footer';
 import OurProcessBanner from './ourProcessSection/OurProcessBanner';
+import OurProcessInfo1 from './ourProcessSection/OurProcessInfo1';
 
 const OurProcess = () => {
     return (
         <div>
             <OurProcessBanner />
+            <OurProcessInfo1/>
 
 
 
---
+++ src/components/ourProcess/ourProcessSection/OurProcessInfo1.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import infoimg1 from '../assets/p4-img-1.jpg'
+
+
+const OurProcessInfo1 = () => {
+    return (
+        <div>
+            <div className='grid grid-cols-1 lg:grid lg:grid-cols-2'>
+                <div>
+                    <img src={infoimg1} alt="" />
+                </div>
+
+
+
+
+                <div className='px-[6rem] lg:py-[5rem] text-end'>
+                    <h1 className='text-4xl font-bold text-black tracking-wide'>EASY BEGINNINGS</h1>
+
+
+                    <div className='flex justify-end'>
+                        <p className='h-[3px] w-[7rem] bg-[#FA471C] my-[0.8rem]'></p>
+                    </div>
+
+
+                    <p className='tracking-wide leading-6 text-black mt-[1rem] mb-[1.5rem]'>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, aenean massa. Cum sociis theme natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus aliquam.
+                    </p>
+
+
+
+                    <button className='border-solid border-[1px] border-[#FA471C] w-[8.4rem] h-[3rem]  hover:w-[8.7rem] hover:h-[3.5rem] ease-in-out duration-300'>
+
+                        <button className='w-[8rem] h-[2.7rem] bg-[#FA471C] text-white font-bold'>BROWSE</button>
+                    </button>
+
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default OurProcessInfo1;
