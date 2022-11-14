1fdc6b9bbbab5d7f9c6de94bec7a056f8674f395
ridoy113 <almamounhridoy113@gmail.com>
2022-11-14 15:41:32 +0600

    portfolio header title creatr'

--- src/components/portfolio/portfolioSection/PortfolioInfo.js
+++ src/components/portfolio/portfolioSection/PortfolioInfo.js
@@ -16,7 +16,19 @@ import infoImg9 from '../assets/port-1-img-12.jpg';
 const PortfolioInfo = () => {
     return (
         <div>
-            
+            <div className='py-[5rem] px-[3rem] md:px-[5rem] lg:px-[8rem]'>
+
+                <div className='text-center text-2xl md:text-3xl lg:text-5xl font-bold text-[#1B1A18]'>
+                    OUR ALL PORTFOLIO FOR CONSUMER
+                </div>
+                <div className='flex justify-center'>
+                    <p className='h-[3px] w-[10rem] lg:h-[4px] lg:w-[25rem] bg-[#FA471C] my-[1rem]'></p>
+                </div>
+
+
+            </div>
+
+
         </div>
     );
 };
