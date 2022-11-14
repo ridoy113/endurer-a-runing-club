dca53123bbc58a8023ca9691691b5f612093bf16
ridoy113 <almamounhridoy113@gmail.com>
2022-11-14 17:50:28 +0600

    project complite

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-### 
+### Live site link
+### https://cheerful-tartufo-923df2.netlify.app/
+
+### My using tools
+* React
+* React-Countup
+* React-Dom
+* React-Icons
+* React-Router
+* Swiper
+* Tailwindcss
+* Daisyui
+
+### Project Overview 
+* Good looking UI.
+* Full front-end site.
+* Fully responsive.
--- src/components/aboutUs/aboutUsSection/AboutUsSlider.js
+++ src/components/aboutUs/aboutUsSection/AboutUsSlider.js
@@ -15,21 +15,21 @@ const AboutUsSlider = () => {
 
                 <div className="carousel w-full">
                     <div id="slide1" className="carousel-item relative w-full">
-                        <img src={slideImg1} className="w-full" />
+                        <img src={slideImg1} className="h-100% w-full" />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❮</a>
                             <a href="#slide2" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❯</a>
                         </div>
                     </div>
                     <div id="slide2" className="carousel-item relative w-full">
-                        <img src={slideImg2} className="w-full" />
+                        <img src={slideImg2} className="h-100% w-full" />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide1" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❮</a>
                             <a href="#slide3" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❯</a>
                         </div>
                     </div>
                     <div id="slide3" className="carousel-item relative w-full">
-                        <img src={slideImg3} className="w-full" />
+                        <img src={slideImg3} className="h-100% w-full" />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide2" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❮</a>
                             <a href="#slide4" className="text-3xl lg:text-7xl font-bold text-[#FA471C]">❯</a>
